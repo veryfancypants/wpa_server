cd285830f82e972c3d2b3eb2e5ec99ba7623def2
veryfancypants <ekuznetsov@live.com>
2018-04-06 15:18:24 -0700

    Fixes & updates

--- do_hashcat.py
+++ do_hashcat.py
@@ -2,17 +2,48 @@
 from threading import Timer
 
 
-def do_spawn_hashcat(a,x,y,z):
+def do_spawn_hashcat(a,x,y,z,ssid):
 	global hashcat_dir
 	v=['.\\hashcat64.exe', '--gpu-temp-disable','-m', '2500', '-w', '3', '-a', a, '../' + x]
 	if type(y) is list:
+		for n in range(0, len(y)):
+			if y[n][:5]=='$SSID':
+				if ssid==None:
+					return
+				if len(y[n])==5:
+					y[n]=ssid
+				else:
+					bracket=y[n][5:]
+					if bracket[0]!='[':
+						y[n]=ssid+bracket
+					else:
+						trail=bracket.find(']')
+						if trail==None:
+							print('Malformed rule')
+							return
+						elif trail==len(bracket)-1:
+							trail=''
+						else:
+							pos=trail
+							trail=bracket[trail+1:]
+							bracket=bracket[1:pos]
+						r=bracket.split(':')
+						if len(r)!=2:
+							print('Malformed rule')
+							return
+						print(r)
+						startpos = 0 if r[0]=='' else int(r[0])
+						endpos = 0 if r[1]=='' else int(r[1])
+						y[n]=ssid[startpos:endpos]+trail
 		v+=y
+		if 'temp.dict' in y or 'temp2.dict' in y or 'ssid.dict' in y or 'ssid_1.dict' in y or 'ssid_2.dict' in y:
+			v.append('--markov-disable')
 	else:
 		v.append(y)
 	of=open("hashcat_status.txt", "w")
 	of.write(x+'\t'+z)
 	of.close()
-	print('Launching hashcat with args', v, 'in', hashcat_dir, os.getcwd())
+	print('Launching', v)
 	#v=['hashcat64.exe']
 	subprocess.run(args=v, cwd=hashcat_dir,shell=True)
 	os.unlink("hashcat_status.txt")
@@ -38,7 +69,7 @@ rules=[]
 
 
 for n in range(0, len(rule_list)):
-	x=rule_list[n].split('\t')
+	x=rule_list[n].replace('\n','').split('\t')
 	name=x[0]
 	condition=x[1]
 	if '=' in condition:
@@ -72,24 +103,23 @@ def pull_ssid(x):
 def gen_ssid_lists(ssid):
 	if ssid==None:
 		return
-	perms1=[ssid[x:y]+'\n' for x in range(0,len(ssid)) for y in range(x+3, len(ssid))]
+	perms1=[ssid[x:y] for x in range(0,len(ssid)) for y in range(x+3, len(ssid)+1)]
 	ssid_l=ssid.lower()
 	ssid_u=ssid.upper()
 	ssid2=ssid_l[0:1].upper()+ssid_l[1:]
-	perms2=[ssid_l[x:y]+'\n' for x in range(0,len(ssid)) for y in range(x+3, len(ssid))]
-	perms3=[ssid_u[x:y]+'\n' for x in range(0,len(ssid)) for y in range(x+3, len(ssid))]
-	perms4=[ssid2[x:y]+'\n' for x in range(0,len(ssid)) for y in range(x+3, len(ssid))]
+	perms2=[ssid_l[x:y] for x in range(0,len(ssid)) for y in range(x+3, len(ssid)+1)]
+	perms3=[ssid_u[x:y] for x in range(0,len(ssid)) for y in range(x+3, len(ssid)+1)]
+	perms4=[ssid2[x:y] for x in range(0,len(ssid)) for y in range(x+3, len(ssid)+1)]
 
 	list1=list(set(perms1))
 	list2=list(set(perms1+perms2+perms3+perms4))
 	list1=[x for x in list1 if len(x)<8 or len(x)==len(ssid)]
 	list2=[x for x in list2 if len(x)<8 or len(x)==len(ssid)]
-
-	of=open('temp.dict','w')
-	of.writelines(list1)
+	of=open(hashcat_dir+'temp.dict','w')
+	of.writelines([x+'\n' for x in list1])
 	of.close()
-	of=open('temp2.dict','w')
-	of.writelines(list2)
+	of=open(hashcat_dir+'temp2.dict','w')
+	of.writelines([x+'\n' for x in list2])
 	of.close()
 
 def test_rule(ap, rule):
@@ -101,6 +131,9 @@ def test_rule(ap, rule):
 	if rule[0]=='oui':
 		return re.fullmatch(rule[1], ap[0][:6].upper())!=None
 	if rule[0]=='vendor':
+		if ap[2]==None:
+			return False
+#		print(rule[1],ap[2])
 		return re.fullmatch(rule[1], ap[2])!=None
 	if rule[0]=='ssid':
 		if ap[3]==None:
@@ -134,6 +167,8 @@ def timer_func():
 		pass
 	for r in rules:
 		for x in aps:
+			#print('AP ', x)
+			ssid=x[3]
 			if not test_rule(x, r[2]):
 				print('file ', x, 'rule ', r, 'Not applicable')
 				continue
@@ -148,12 +183,12 @@ def timer_func():
 			of=open(hashcat_dir+'known_passwords.txt','w')
 			of.writelines([x[2] for x in e if len(x)>=3])
 			of.close()
-			gen_ssid_lists(x[3])
-			do_spawn_hashcat(r[3], x[1], r[4], x[0])
+			gen_ssid_lists(ssid)
+			do_spawn_hashcat(r[3], x[1], r[4], r[0], ssid)
+			time.sleep(5.0)
 			ad=open("ap_done.txt", "a")
 			ad.write(x[1]+'\t'+r[0]+'\n')
 			ad.close()
-
 while True:
 	timer_func()
 	time.sleep(5.0)
